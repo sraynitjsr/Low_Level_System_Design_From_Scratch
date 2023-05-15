bf398c5a0b62fb5870cc5cb2ced0924bec639b6e
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-15 23:43:12 +0530

    Builder Design Pattern

--- com/sraynitjsr/system/design/Main.java
+++ com/sraynitjsr/system/design/Main.java
@@ -3,5 +3,6 @@ package com.sraynitjsr.system.design;
 public class Main {
     public static void main(String[] args) {
         System.out.println("System Design From Scratch");
+        System.out.println("Builder Design Pattern");
     }
 }

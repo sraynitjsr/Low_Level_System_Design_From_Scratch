4803fc990a4e823a2864bf60a1d0f9d589d0c3df
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-15 23:45:46 +0530

    Builder Design Pattern in Java

--- com/sraynitjsr/system/design/Main.java
+++ com/sraynitjsr/system/design/Main.java
@@ -1,8 +1,12 @@
 package com.sraynitjsr.system.design;
 
+import com.sraynitjsr.system.design.lld.creational.Builder;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("System Design From Scratch");
+        
         System.out.println("Builder Design Pattern");
+        Builder.start();
     }
 }
--- com/sraynitjsr/system/design/lld/creational/Builder.java
+++ com/sraynitjsr/system/design/lld/creational/Builder.java
@@ -1,5 +1,7 @@
 package com.sraynitjsr.system.design.lld.creational;
 
 public class Builder {
-    
+    public static void start() {
+        System.out.println("Builder Design Pattern Using Fluent Interface Concept");
+    }
 }

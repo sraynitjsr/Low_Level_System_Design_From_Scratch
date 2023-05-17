8ade00eadeb3890f4494975aa1af8f9293f22c60
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-17 07:26:53 +0530

    Singleton Design Pattern

--- com/sraynitjsr/system/design/Main.java
+++ com/sraynitjsr/system/design/Main.java
@@ -1,11 +1,14 @@
 package com.sraynitjsr.system.design;
 
-import com.sraynitjsr.system.design.lld.creational.Builder;
+import com.sraynitjsr.system.design.lld.creational.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("System Design From Scratch");
         
+        System.out.println("Singleton Design Pattern");
+        Singleton.start();
+
         System.out.println("Builder Design Pattern");
         Builder.start();
     }
--- com/sraynitjsr/system/design/lld/creational/Singleton.java
+++ com/sraynitjsr/system/design/lld/creational/Singleton.java
@@ -1,5 +1,25 @@
+package com.sraynitjsr.system.design.lld.creational;
+
+class MyDatabase {
+    private static MyDatabase instance;
+
+    private MyDatabase() {
+    }
+
+    synchronized public static MyDatabase getInstance() {
+        if (instance == null) {
+            instance = new MyDatabase();
+        }
+        return instance;
+    }
+}
+
 public class Singleton {
-    public static void start() {
-        System.out.println("Singleton Design Pattern");
+    public static void start() {        
+        MyDatabase obj1 = MyDatabase.getInstance();
+        System.out.println(obj1.getClass().getName() + " Obj1");
+
+        MyDatabase obj2 = MyDatabase.getInstance();
+        System.out.println(obj2.getClass().getName() + " Obj2");
     }
 }

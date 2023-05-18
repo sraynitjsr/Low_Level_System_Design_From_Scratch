cd68cf4d752a135a17ae970dc4d366a33f8b262b
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-18 23:33:52 +0530

    Strategy Design Pattern

--- com/sraynitjsr/system/design/Main.java
+++ com/sraynitjsr/system/design/Main.java
@@ -2,6 +2,7 @@ package com.sraynitjsr.system.design;
 
 import com.sraynitjsr.system.design.lld.structural.*;
 import com.sraynitjsr.system.design.lld.creational.*;
+import com.sraynitjsr.system.design.lld.behavioral.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,5 +22,8 @@ public class Main {
 
         System.out.println("Facade Design Pattern");
         Facade.start();
+
+        System.out.println("Strategy Design Pattern");
+        Strategy.start();
     }
 }
---
+++ com/sraynitjsr/system/design/lld/behavioral/Strategy.java
@@ -0,0 +1,7 @@
+package com.sraynitjsr.system.design.lld.behavioral;
+
+public class Strategy {
+    public static void start() {
+        System.out.println("Strategy Design Pattern");
+    }
+}

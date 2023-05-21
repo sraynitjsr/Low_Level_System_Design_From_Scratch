1dd9f9d4a74dd1810afdc86b1b31e96938f559b3
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-21 23:58:36 +0530

    Decorator Design Pattern

--- com/sraynitjsr/system/design/lld/structural/Decorator.java
+++ com/sraynitjsr/system/design/lld/structural/Decorator.java
@@ -1,7 +1,67 @@
 package com.sraynitjsr.system.design.lld.structural;
 
+interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape {
+
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
+
+class Circle implements Shape {
+
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
+
+abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw() {
+        decoratedShape.draw();
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape) {
+        System.out.println("Border Color: Red");
+    }
+}
+
 public class Decorator {
     public static void start() {
         System.out.println("********Decorator Design Pattern********");
+
+        Shape circle = new Circle();
+
+        Shape redCircle = new RedShapeDecorator(new Circle());
+
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw();
     }
 }

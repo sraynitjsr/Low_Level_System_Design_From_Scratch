901e7d0bddf8bf5f4943f06339269b7ebe02e14e
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-22 17:15:37 +0530

    Adapter Design Pattern

--- com/sraynitjsr/system/design/lld/structural/Adapter.java
+++ com/sraynitjsr/system/design/lld/structural/Adapter.java
@@ -1,7 +1,89 @@
 package com.sraynitjsr.system.design.lld.structural;
 
+interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
+
+interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
+
+class VlcPlayer implements AdvancedMediaPlayer {
+
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: " + fileName);
+    }
+
+    public void playMp4(String fileName) {
+        System.out.println("None of my concerns, I play MP4 only");
+    }
+}
+
+class Mp4Player implements AdvancedMediaPlayer {
+
+    public void playVlc(String fileName) {
+        System.out.println("None of my concerns, I play VLC only");
+    }
+
+    public void playMp4(String fileName) {
+        System.out.println("Playing mp4 file. Name: " + fileName);
+    }
+}
+
+class MediaAdapter implements MediaPlayer {
+
+    AdvancedMediaPlayer advancedMusicPlayer;
+
+    public MediaAdapter(String audioType) {
+
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer = new VlcPlayer();
+
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer = new Mp4Player();
+        }
+    }
+
+    public void play(String audioType, String fileName) {
+
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer.playVlc(fileName);
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer.playMp4(fileName);
+        }
+    }
+}
+
+class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    public void play(String audioType, String fileName) {
+
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        }
+
+        else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        }
+
+        else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
+
 public class Adapter {
     public static void start() {
         System.out.println("********Adapter Design Pattern********");
+        
+        AudioPlayer audioPlayer = new AudioPlayer();
+        audioPlayer.play("mp3", "beyond the horizon.mp3");
+        audioPlayer.play("mp4", "alone.mp4");
+        audioPlayer.play("vlc", "far far away.vlc");
+        audioPlayer.play("avi", "mind me.avi");
     }
 }

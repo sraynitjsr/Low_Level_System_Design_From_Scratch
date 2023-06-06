bafa0db6b7a9944c54a1caa228ba7ad9e3456890
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-06 19:14:30 +0530

    Flyweight Design Pattern Implementation

--- com/sraynitjsr/system/design/Main.java
+++ com/sraynitjsr/system/design/Main.java
@@ -7,7 +7,7 @@ import com.sraynitjsr.system.design.lld.behavioral.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("********System Design From Scratch********");
-                
+
         System.out.println();
         Observer.start();
         System.out.println();
--- com/sraynitjsr/system/design/lld/structural/Flyweight.java
+++ com/sraynitjsr/system/design/lld/structural/Flyweight.java
@@ -1,7 +1,52 @@
 package com.sraynitjsr.system.design.lld.structural;
 
+import java.util.*;
+
+// Flyweight interface
+interface Shape {
+    void draw();
+}
+
+// Concrete flyweight class
+class Circle implements Shape {
+    private String color;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    public void draw() {
+        System.out.println("Drawing a circle with color: " + color);
+    }
+}
+
+// Flyweight factory
+class ShapeFactory {
+    private static final Map<String, Shape> circleMap = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating a new circle with color: " + color);
+        }
+
+        return circle;
+    }
+}
+
+// Client
 public class Flyweight {
+    private static final String[] colors = { "Red", "Blue", "Green", "Yellow", "Black" };
+
     public static void start() {
         System.out.println("********Flyweight Design Pattern********");
+        for (int i = 0; i < 10; i++) {
+            String newColor = colors[(int) (Math.random() * colors.length)];
+            Circle circle = (Circle) ShapeFactory.getCircle(newColor);
+            circle.draw();
+        }
     }
 }
